c35705aec84a4cc85eb491d67b33be6adc15c8e9
Dhanushka <dhanushkamcr@gmail.com>
2025-08-03 13:16:22 -0500

    Conditional rendering

--- src/FunctionStateTest.jsx
+++ src/FunctionStateTest.jsx
@@ -2,6 +2,7 @@ import { useState } from "react"
 
 function FunctionStateTest() {
     const[name, setName] = useState("Dhanushka inside function");
+    const [isLoggedIn, setIsLoggedIn] = useState(false);
     
     const changeName = () =>{
         setName("Changed tha name using arrow function and setState")
@@ -10,6 +11,14 @@ function FunctionStateTest() {
     function changeNameFunction() {
         setName("Changed tha name using function")
     }
+    
+    const logInClick = () =>{
+        setIsLoggedIn(true);
+    }
+    
+    const logOutClick = () =>{
+        setIsLoggedIn(false);
+    }
 
     return(
         <>
@@ -17,6 +26,14 @@ function FunctionStateTest() {
             <p>Name: {name}</p>
             <button onClick={changeNameFunction}>Change Name</button>
             <button onClick={changeName}>Change Name using Function</button>
+
+            <p>======================================</p>
+            <p>{isLoggedIn ? "Logged In" : "Logged Out"}</p>
+            {!isLoggedIn ? (
+                <button onClick={logInClick}>Log In</button>
+            ) : (
+                <button onClick={logOutClick}>Log Out</button>
+            )}
         </>
     )
 }
